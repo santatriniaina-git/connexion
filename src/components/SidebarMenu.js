a7ca45a96851c8570abc775e5e4eceb8f1b6bdc1
Tohaina <tohainatsaroana@gmail.com>
2023-04-19 22:16:24 +0300

    amélioreation 4

--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -8,12 +8,27 @@ function SidebarMenu({ steps, currentStep, setCurrentStep }) {
           <li
             key={step.name}
             className={`w-full flex justify-start items-center mb-4 py-2 rounded-lg font-medium cursor-pointer transition duration-200 
-              ${index + 1 === currentStep || index + 1 < currentStep ? "text-black" : "text-[#B0BEC5]"}
-              ${step.name === "" && "hidden"} uppercase pl-14
-            `}
+            ${index + 1 === currentStep || index + 1 < currentStep ? "text-black" : "text-[#B0BEC5]"}
+            ${step.name === "" && "hidden"} uppercase pl-14
+        `}
             onClick={() => setCurrentStep(index + 1)}
           >
-            {step.name}
+            {step.name !== "" && (
+              <>
+                {steps[index - 1]?.name !== "" && index !== 0 ? (
+                  <>
+                    {index + 1 === currentStep || index + 1 < currentStep ? (
+                      <span className="font-bold text-black">X</span>
+                    ) : (
+                      <span className="font-bold text-[#B0BEC5]">Y</span>
+                    )}
+                  </>
+                ) : (
+                  <span className="font-bold text-black">X</span>
+                )}
+                <span>{step.name}</span>
+              </>
+            )}
           </li>
         ))}
       </ul>
