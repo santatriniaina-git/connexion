6bf188fbe4f5637909cf8fbe0ab6b479a0e62a8c
Tohaina <tohainatsaroana@gmail.com>
2023-04-20 20:04:32 +0300

    amélioration

--- src/components/tabs/Step2.js
+++ src/components/tabs/Step2.js
@@ -8,7 +8,7 @@ function Step2({ nextStep }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-x-20">
+    <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-x-20  gap-y-11">
       <div>
         <label htmlFor="field1">Nom commercial *</label>
         <input
