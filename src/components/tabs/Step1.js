20009ec317ebacd1ab8eae9587ead75ae47686d8
Tohaina <tohainatsaroana@gmail.com>
2023-04-20 22:25:03 +0300

    amélioration

--- src/components/tabs/Step1.js
+++ src/components/tabs/Step1.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ButtonConnexion from '../ButtonConnexion';
 
 function Step1({ nextStep }) {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        nextStep();
-      };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <ButtonConnexion/>
-      </form>
-    );
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    nextStep();
+  };
+
+  return (
+    <div className="flex justify-between">
+      <div className="w-1/2 mr-4">
+        <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies mi in eros pharetra, sed aliquam elit volutpat. Nulla facilisi.</p>
+        <form onSubmit={handleSubmit}>
+          <ButtonConnexion />
+        </form>
+      </div>
+      <div className="w-1/2">
+        <img src="" alt="image description" />
+      </div>
+    </div>
+  );
+}
 
 export default Step1;
-  
