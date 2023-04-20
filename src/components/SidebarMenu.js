f6d7128ad6e853d6a0ea4ddd9eafa9f9f1efb2e2
Tohaina <tohainatsaroana@gmail.com>
2023-04-20 08:50:18 +0300

    amélioration 5

--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -8,11 +8,9 @@ function SidebarMenu({ steps, currentStep, setCurrentStep }) {
           const isSelectedStep = index + 1 === currentStep;
           const isPastStep = index + 1 < currentStep;
           const isHiddenStep = step.name === "";
-          const isPreviousStepNameValid = steps[index - 1]?.name && index !== 0;
           const isSelectedOrPastClass = isSelectedStep || isPastStep ? "text-black" : "text-[#B0BEC5]";
-          const selectedOrPastIcon = isSelectedStep || isPastStep ? "X" : "Y";
-          const stepIcon = isPreviousStepNameValid
-            ? <span className="font-bold">{selectedOrPastIcon}</span>
+          const stepIcon = steps[index - 1]?.name && index !== 0
+            ? <span className="font-bold">{isSelectedStep || isPastStep ? "X" : "Y"}</span>
             : <span className="font-bold">X</span>;          
           return (
             <li
