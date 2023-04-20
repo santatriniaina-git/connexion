d2b3462e759762a159e6310421e7688041b11f03
Tohaina <tohainatsaroana@gmail.com>
2023-04-20 16:25:53 +0300

    button

--- src/components/ButtonConnexion.js
+++ src/components/ButtonConnexion.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function ButtonConnexion() {
   return (
-    <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-4">
-          Next
+    <button type="submit" className="bg-black text-white font-bold py-3.5 px-4 mt-4 rounded-md">
+          Continuer
     </button>
   );
 }
