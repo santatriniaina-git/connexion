674c252e40e3444ef636d4e503544ee0f5c552b5
Tohaina <tohainatsaroana@gmail.com>
2023-04-20 20:52:44 +0300

    amélioration

--- src/components/tabs/Step3.js
+++ src/components/tabs/Step3.js
@@ -1,20 +1,89 @@
-import React from 'react';
-import ButtonConnexion from '../ButtonConnexion';
+import React from "react";
+import ButtonConnexion from "../ButtonConnexion";
 
 function Step3({ nextStep }) {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        nextStep();
-      };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Step3</label>
-        <input type="text" id="name" name="name" className="block w-full mt-2" required />
-        <ButtonConnexion/>
-      </form>
-    );
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    nextStep();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-x-20 gap-y-11">
+      <div>
+        <label htmlFor="field1">Nom commercial *</label>
+        <input
+          type="text"
+          id="field1"
+          name="field1"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="field2">Industrie *</label>
+        <input
+          type="text"
+          id="field2"
+          name="field2"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="field3">Courriel général de l'entreprise *</label>
+        <input
+          type="text"
+          id="field3"
+          name="field3"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="field4">Téléphone de l'entreprise *</label>
+        <input
+          type="text"
+          id="field4"
+          name="field4"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="field5">Adresse *</label>
+        <input
+          type="text"
+          id="field5"
+          name="field5"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="field6">Adresse Complémentaire </label>
+        <input
+          type="text"
+          id="field6"
+          name="field6"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="field7">Fuseau horaire *</label>
+        <input
+          type="text"
+          id="field7"
+          name="field7"
+          className="border border-gray-300 rounded-md p-2 w-full"
+          required
+        />
+      </div>
+      <div className="col-span-2">
+        <ButtonConnexion />
+      </div>
+    </form>
+  );
+}
 
 export default Step3;
-  
