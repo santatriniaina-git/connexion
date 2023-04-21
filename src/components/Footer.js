6e3b12c298a5f4de379866e569b45e55b0eb7ce7
Tohaina <tohainatsaroana@gmail.com>
2023-04-21 16:58:48 +0300

    header et Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className='absolute bottom-0 right-0 w-3/4 h-16 flex items-center justify-end'>
+    <div className='absolute bottom-0 right-0 w-3/4 h-16 text-white bg-red-400 flex items-center justify-end'>
       <h1>FOOTER</h1>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='absolute top-0 right-0 w-3/4 h-16 flex items-center justify-end'>
+    <div className='absolute top-0 right-0 w-3/4 h-16 text-white bg-red-400 flex items-center justify-end'>
       <h1>HEADER</h1>
     </div>
   );
