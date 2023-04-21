f7ef225a44af6e2d3f87404f6ef90e576416e630
Tohaina <tohainatsaroana@gmail.com>
2023-04-21 14:41:13 +0300

    amélioration 6

--- src/components/tabs/Step1.js
+++ src/components/tabs/Step1.js
@@ -3,7 +3,7 @@ import ButtonConnexion from '../ButtonConnexion';
 import image from '../../assets/images.PNG';
 
 function Step1({ nextStep }) {
-  const listItems1 = [
+  const productFeatures = [
     'Nous proposons une large gamme de produits pour répondre à tous vos besoins.',
     'Nos produits sont fabriqués avec des matériaux de qualité supérieure.',
     'Nous nous engageons à fournir un service client exceptionnel à tous nos clients.',
@@ -11,7 +11,7 @@ function Step1({ nextStep }) {
     'Notre équipe expérimentée est là pour vous aider à chaque étape du processus.'
   ];
 
-  const listItems2 = [
+  const qualityFeatures = [
     'Nous effectuons des tests rigoureux sur tous nos produits pour garantir leur qualité.',
     'Nous utilisons des matériaux de qualité supérieure pour fabriquer tous nos produits.',
     'Nous sommes fiers d\'offrir une garantie de qualité sur tous nos produits.',
@@ -30,7 +30,7 @@ function Step1({ nextStep }) {
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-4 uppercase">Avant de créer un compte, quelques petites précisions sur Altee</h2>
           <ul className="list-disc pl-6">
-            {listItems1.map((item, index) => (
+            {productFeatures.map((item, index) => (
               <li className="mb-2" key={index}>{item}</li>
             ))}
           </ul>
@@ -38,7 +38,7 @@ function Step1({ nextStep }) {
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-4 uppercase">Nous nous distinguons principalement par la qualité du produit</h2>
           <ul className="list-disc pl-6">
-            {listItems2.map((item, index) => (
+            {qualityFeatures.map((item, index) => (
               <li className="mb-2" key={index}>{item}</li>
             ))}
           </ul>
