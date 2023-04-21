bac3b575f42a670044eca0e7a5e2d539f2fe666b
Tohaina <tohainatsaroana@gmail.com>
2023-04-21 12:57:17 +0300

    amélioration 6

--- src/components/tabs/Step1.js
+++ src/components/tabs/Step1.js
@@ -9,15 +9,15 @@ function Step1({ nextStep }) {
   };
 
   return (
-    <div className="flex justify-between">
-      <div className="w-1/2 mr-8">
+    <div className="mx-auto h-screen flex justify-between">
+      <div className="w-1/2 mr-8 flex flex-col justify-center">
         <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies mi in eros pharetra, sed aliquam elit volutpat. Nulla facilisi.</p>
         <form onSubmit={handleSubmit}>
           <ButtonConnexion />
         </form>
       </div>
-      <div className="w-1/2">
-        <img src={image} alt="description" />
+      <div className="w-1/2 h-full flex items-center justify-center">
+        <img src={image} alt="description" className="object-cover h-full" />
       </div>
     </div>
   );
