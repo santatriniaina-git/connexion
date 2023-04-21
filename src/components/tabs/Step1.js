182ec6a50bf235ff6d2bcb7e48954f24c8244d91
Tohaina <tohainatsaroana@gmail.com>
2023-04-21 14:29:54 +0300

    amélioration 6

--- src/components/tabs/Step1.js
+++ src/components/tabs/Step1.js
@@ -3,6 +3,22 @@ import ButtonConnexion from '../ButtonConnexion';
 import image from '../../assets/images.PNG';
 
 function Step1({ nextStep }) {
+  const listItems1 = [
+    'Nous proposons une large gamme de produits pour répondre à tous vos besoins.',
+    'Nos produits sont fabriqués avec des matériaux de qualité supérieure.',
+    'Nous nous engageons à fournir un service client exceptionnel à tous nos clients.',
+    'Nous sommes fiers d\'offrir des prix compétitifs sur tous nos produits.',
+    'Notre équipe expérimentée est là pour vous aider à chaque étape du processus.'
+  ];
+
+  const listItems2 = [
+    'Nous effectuons des tests rigoureux sur tous nos produits pour garantir leur qualité.',
+    'Nous utilisons des matériaux de qualité supérieure pour fabriquer tous nos produits.',
+    'Nous sommes fiers d\'offrir une garantie de qualité sur tous nos produits.',
+    'Nous nous engageons à fournir des produits qui répondent aux normes les plus élevées de qualité.',
+    'Nous sommes constamment à la recherche de moyens d\'améliorer la qualité de nos produits.'
+  ];
+
   const handleSubmit = (event) => {
     event.preventDefault();
     nextStep();
@@ -14,21 +30,17 @@ function Step1({ nextStep }) {
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-4 uppercase">Avant de créer un compte, quelques petites précisions sur Altee</h2>
           <ul className="list-disc pl-6">
-            <li className="mb-2">Nous proposons une large gamme de produits pour répondre à tous vos besoins.</li>
-            <li className="mb-2">Nos produits sont fabriqués avec des matériaux de qualité supérieure.</li>
-            <li className="mb-2">Nous nous engageons à fournir un service client exceptionnel à tous nos clients.</li>
-            <li className="mb-2">Nous sommes fiers d'offrir des prix compétitifs sur tous nos produits.</li>
-            <li className="mb-2">Notre équipe expérimentée est là pour vous aider à chaque étape du processus.</li>
+            {listItems1.map((item, index) => (
+              <li className="mb-2" key={index}>{item}</li>
+            ))}
           </ul>
         </div>
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-4 uppercase">Nous nous distinguons principalement par la qualité du produit</h2>
           <ul className="list-disc pl-6">
-            <li className="mb-2">Nous effectuons des tests rigoureux sur tous nos produits pour garantir leur qualité.</li>
-            <li className="mb-2">Nous utilisons des matériaux de qualité supérieure pour fabriquer tous nos produits.</li>
-            <li className="mb-2">Nous sommes fiers d'offrir une garantie de qualité sur tous nos produits.</li>
-            <li className="mb-2">Nous nous engageons à fournir des produits qui répondent aux normes les plus élevées de qualité.</li>
-            <li className="mb-2">Nous sommes constamment à la recherche de moyens d'améliorer la qualité de nos produits.</li>
+            {listItems2.map((item, index) => (
+              <li className="mb-2" key={index}>{item}</li>
+            ))}
           </ul>
         </div>
         <form onSubmit={handleSubmit}>
