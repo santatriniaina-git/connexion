eb9f58fa5bf3d283527c6dbc2eb45dfc789b044b
Tohaina <tohainatsaroana@gmail.com>
2023-04-25 10:41:25 +0300

    every

--- src/components/tabs/Step1.js
+++ src/components/tabs/Step1.js
@@ -24,7 +24,7 @@ function FeatureList({ features }) {
     <ul className="list-disc">
       {features.map((item, index) => (
         <li
-          className="mb-2 py-[1.063rem] pl-4 pr-4 rounded-md bg-gray-100 flex items-center"
+          className="mb-2 py-[1.063rem] pl-5 pr-5 rounded-md bg-gray-100 flex items-center"
           key={index}
         >
           <p className="mr-3">
