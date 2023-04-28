8eab20594663303d9195e8d3b346df32ae426223
Santatra <vs@alternateeve.com>
2023-04-28 17:33:45 +0300

    vos besoins

--- src/components/tabs/Step4.js
+++ src/components/tabs/Step4.js
@@ -2,7 +2,7 @@ import React from "react";
 import BigDisclosure from "../disclosure/BigDisclosure";
 import ButtonConnexion from "../ButtonConnexion";
 
-const labels = [
+const labelsNeed = [
   "Espace personnel",
   "Dossiers employés",
   "Suivi des heures et pointages",
@@ -19,22 +19,28 @@ const labels = [
   "Horaires de travail et vacances"
 ];
 
-function NumberedLabels() {
+const labelsCareer = [
+  "Site carrière avancée",
+  "Gestion de recrutement",
+  "Statistiques et Analytique",
+  "Jobboard sur mesure"
+];
+
+function NumberedLabels({labels}) {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-3 gap-2">
       {labels.map((label, index) => (
         <div key={index} className="box-border flex flex-row items-center p-15 gap-10 w-[20rem] h-[3.375rem] bg-gray-100 border border-gray-500 rounded-md mt-3 relative">
-            <div className="flex items-center space-x-4 ">
-            <span>
+            <div className="flex items-center space-x-2 ">
+            <span className="">
               x
             </span>
           <span className="font-medium text-sm leading-6 text-primary flex-none order-0 flex-grow font-poppins">{label}</span>
             </div>
           <span className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-3">
-       y
-      </span>
-          </div>
-
+            y
+          </span>
+        </div>
       ))}
     </div>
   );
@@ -47,23 +53,24 @@ function Step4({ nextStep }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="grid grid-row-2 gap-x-20 gap-y-11 items-center">
+    <form onSubmit={handleSubmit} className="grid grid-row-2 gap-x-20 gap-y-11 items-center p-[9.313rem]">
       <div className="flex text-[3.75rem] font-bebas font-normal items-center" >VOS BESOINS</div>
-      <div className="flex flex-col space-y-6 ">
+      <div className="flex flex-col space-y-2">
         <BigDisclosure
-          title="Gestion des ressources humaines"
+          title="Gestion des ressources humaines" 
           classParent="bg-white my-2 px-2 py-2 h-auto"
         >
-          <NumberedLabels />
+          <NumberedLabels labels={labelsNeed} />
         </BigDisclosure>
         <BigDisclosure
           title="Recrutement et site carrière"
-          classParent="bg-white my-2 px-2 py-2 h-26"
+          classParent="bg-white my-2 px-1 py-2 h-26"
         >
-          <p className="h-full">text2</p>
+          <NumberedLabels labels={labelsCareer} />
         </BigDisclosure>
       </div>
-      <div className="grid grid-cols-2 gap-x-20 gap-y-11">
+      <div>
+      <div className="grid grid-cols-2 gap-x-20 gap-y-11 mr-[16rem]">
         <div>
           <label htmlFor="field1">Code partenaire (facultatif)</label>
           <input
@@ -86,6 +93,9 @@ function Step4({ nextStep }) {
             required
           />
         </div>
+      </div>
+    </div>
+      <div>
         <div className="flex items-center">
           <div className="flex items-center">
             <label htmlFor="terms-conditions">
